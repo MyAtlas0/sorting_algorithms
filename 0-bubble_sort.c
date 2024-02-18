75295b7485b626bede2cfe2f3a6b2b3d0ea90f7d
MyAtlas0 <AbdullahiOlawale09@gmail.com>
2024-02-18 13:53:33 -0800

    TASK 0 (update) a& README (update)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,21 @@
 #include "sort.h"
 
+/*
+ * bubble_sort - sorts an array of integers using the Bubble Sort algorithm.
+ *
+ * @array: pointer to the array to be sorted.
+ *
+ * @size: number of elements in the array.
+ *
+ * Description: This function sorts the elements of the array in place.
+ *		It iterates through the array multiple times,
+ *		compares adjacent elements and swap them if unsorted.
+ *		This process is repeated until the entire array is sorted.
+ */
+
 void bubble_sort(int *array, size_t size)
 {
 	int swapped = 1;
-	/**size_t last_swappedIndex = 0;**/
 	size_t i;
 	int tmp;
 
@@ -23,10 +35,8 @@ void bubble_sort(int *array, size_t size)
 				array[i] = array[i + 1];
 				array[i + 1] = tmp;
 				swapped = 1;
-				/**last_swappedIndex = i + 1;**/
 				print_array(array, size);
 			}
 		}
-		/**size = last_swappedIndex;**/
 	}
 }
